e884ed435f82faeb4d1a5de75f7cdd86517bb79b
frhn16 <faroomirz@hotmail.com>
2022-10-02 12:53:29 +0100

    Added message flashing when user makes new account incorrectly

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
@@ -23,13 +23,13 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
         if len(email) < 4:
-            pass
+            flash("Email must be greater than 3 characters.", category="error")
         elif len(firstName) < 2:
-            pass
-        elif password1!=password2:
-            pass
-        elif len(password1):
-            pass
+            flash("First name must be greater than 1 character.", category="error")
+        elif password1 != password2:
+            flash("Passwords don't match.", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category="error")
         else:
-            pass
+            flash("Account created!", category="success")
     return render_template("sign_up.html")
